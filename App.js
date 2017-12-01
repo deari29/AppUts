f96247d2998b3b85857b27804565fa2724f5f7e2
I Gede Ari Putra <34137626+deari29@users.noreply.github.com>
2017-12-01 16:26:47 +0800

    AppUts
    APLIKASI UTS TO DO LIST PEMROGRAMAN BERBASIS MOBILE

---
+++ App.js
@@ -0,0 +1,15 @@
+/**
+ * Written by I Gede Ari Putra
+ */
+import React from 'react';
+import Main from './app/components/Main';
+
+export default class App extends React.Component {
+  render() {
+    return (
+
+      <Main />
+      
+    );
+  }
+}
